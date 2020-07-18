677e281af0f26f8a042e518a5ab849d8cc000ce8
erickgam <ernsgam@hotmail.com>
2020-07-18 00:53:42 +0000

    OkSections

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,6 +16,67 @@
 
 $(document).on('turbolinks:load', function(){
   $.rails.refreshCSRFTokens();
+  function goready() {
+    if(window.location.pathname == "/b/" || window.location.pathname == "/b"){
+      div_position('div1',6500);
+      div_position('div2',7200);
+      div_position('div3',4200);
+      div_position('div4',5100);
+      div_position('div5',6000);
+      div_position('div6',7400);
+      div_position('div7',8000);
+      div_position('div8',9200);
+      //intervals for moving divs:
+let interval1 = setInterval(function () {
+  div_position("div1", 5500);
+}, 8500);
+let interval2 = setInterval(function () {
+  div_position("div2", 5200);
+}, 4200);
+let interval3 = setInterval(function () {
+  div_position("div3", 4200);
+}, 8200);
+let interval4 = setInterval(function () {
+  div_position("div4", 5100);
+}, 10100);
+let interval5 = setInterval(function () {
+  div_position("div5", 6000);
+}, 5000);
+let interval6 = setInterval(function () {
+  div_position("div6", 6400);
+}, 7400);
+let interval7 = setInterval(function () {
+  div_position("div7", 6000);
+}, 9000);
+let interval8 = setInterval(function () {
+  div_position("div8", 6200);
+}, 15200);
+
+
+    }
+
+  }
+    //to move the div to specific location:
+function div_position(id, seconds) {
+if(window.location.pathname == "/b/" || window.location.pathname == "/b"){  let myDiv = document.getElementById(id);
+
+  let window_width = window.innerWidth-100;
+  let window_height = window.innerHeight-100;
+
+  let x = window_width * Math.random();
+  myDiv.style.left = x + "px";
+
+//  let y = window_height * (Math.random() * (0.6 - 0.3) + 0.3);
+
+    let y = Math.random() * (330 - 180) + 180;
+
+    myDiv.style.top = y + "px";
+
+  myDiv.style.transition =
+    "top " + seconds + "ms ease,left " + seconds + "ms ease";}
+}
+goready()
+
 })
 
 document.addEventListener("turbolinks:before-cache", function() {
